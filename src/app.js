9e08a7ea837adb4010836630cc0ad4748f833a22
ahmedzaky2 <ahmedzakymouad303@gmail.com>
2025-08-29 22:54:20 +0300

    Test Changes

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ function greet(name) {
 
     module.exports = greet;
 
-    if (require.main === module) {
+    // if (require.main === module) {
 
-        console.log(greet("World"));
-        
-    }
+    //     console.log(greet("World"));
+
+    // }
